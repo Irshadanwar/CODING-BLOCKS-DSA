eb812dd88d3d38f074b5f67a12d85419fbabb244
MD IRSHAD ANWAR <83213208+Irshadanwar@users.noreply.github.com>
2025-05-03 14:25:20 +0530

    Update fibo.java

--- JAVA_CRUX_CB/src/lec_5/fibo.java
+++ JAVA_CRUX_CB/src/lec_5/fibo.java
@@ -18,3 +18,31 @@ public class fibo {
 	}
 
 }
+
+
+
+
+/* using for loop:
+
+package lec5;
+
+import java.util.*;
+
+public class fibo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a=0, b=1, c=0;
+		for (int i = 0; i < n; i++) {
+			c = a+b;
+			a=b;
+			b=c;
+			
+		}
+		System.out.println(a);
+		
+
+	}
+
+}
+*/
